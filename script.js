1eb480e4f76336086bbfde0955e7bdd1eca8b3e7
Gustavo Marialva <gmarialva@gmail.com>
2023-02-08 09:38:17 -0300

    async await

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
-var consultaCep = fetch("https://viacep.com.br/ws/01001000/json/")
-  .then((resposta) => resposta.json())
-  .then((r) => {
-    if (r.erro) {
-      throw Error("Esse cep não existe!");
-    } else console.log(r);
-  })
-  .catch((erro) => console.log(erro))
-  .finally((mensagem) => console.log("Processamento concluído!"));
+async function buscaEndereco() {
+  var consultaCep = await fetch("https://viacep.com.br/ws/01001000/json/");
+  var consultaCepConvertida = await consultaCep.json();
+  console.log(consultaCepConvertida);
+}
 
-console.log(consultaCep);
+// .then((resposta) => resposta.json())
+// .then((r) => {
+//   if (r.erro) {
+//     throw Error("Esse cep não existe!");
+//   } else console.log(r);
+// })
+// .catch((erro) => console.log(erro))
+// .finally((mensagem) => console.log("Processamento concluído!"));
+
+buscaEndereco();
