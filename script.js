e923d27ca3a5e631e518d0d05d86c8db687d67f9
Gustavo Marialva <gmarialva@gmail.com>
2023-02-08 10:13:15 -0300

    adiciona eventListener no cep e autocomplete

--- script.js
+++ script.js
@@ -5,12 +5,27 @@ async function buscaEndereco(cep) {
     if (consultaCepConvertida.erro) {
       throw Error("CEP não existente!");
     }
+    var cidade = document.getElementById("cidade");
+    var logradouro = document.getElementById("endereco");
+    var bairro = document.getElementById("bairro");
+    var estado = document.getElementById("estado");
+
+    cidade.value = consultaCepConvertida.localidade;
+    logradouro.value = consultaCepConvertida.logradouro;
+    bairro.value = consultaCepConvertida.bairro;
+    estado.value = consultaCepConvertida.uf;
+
     console.log(consultaCepConvertida);
     return consultaCepConvertida;
   } catch (erro) {
     console.log(erro);
   }
 }
+
+var cep = document.getElementById("cep");
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+
+// focusout - quando a possa clica fora do campo. Ex campo do CEP, qd a pessoa clica fora do campo ele ativa a função
 
 // .then((resposta) => resposta.json())
 // .then((r) => {
@@ -21,6 +36,6 @@ async function buscaEndereco(cep) {
 // .catch((erro) => console.log(erro))
 // .finally((mensagem) => console.log("Processamento concluído!"));
 
-let ceps = ["24240460", "22220080"];
-let conjuntoCeps = ceps.map((valores) => buscaEndereco(valores));
-Promise.all(conjuntoCeps).then((respostas) => console.log(respostas));
+// let ceps = ["24240460", "22220080"];
+// let conjuntoCeps = ceps.map((valores) => buscaEndereco(valores));
+// Promise.all(conjuntoCeps).then((respostas) => console.log(respostas));
